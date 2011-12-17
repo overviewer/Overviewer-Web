5ea393d489c1ceba51ec7701b681af6311d23ecf
Aaron Griffith <aargri@gmail.com>
2011-12-17 06:26:08 -0500

    post-hook now passes in the relevant ref

--- oo_extra/views.py
+++ oo_extra/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse, Http404
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+import json
 import os
 
 @csrf_exempt
 def update(request):
     if request.method == 'POST' and 'payload' in request.POST and 'key' in request.GET and request.GET['key'] == settings.UPDATE_HOOK_KEY:
-        os.system(settings.UPDATE_HOOK_COMMAND)
+        data = json.load(request)
+        ref = data['ref']
+        os.system("%s %s" % (settings.UPDATE_HOOK_COMMAND, ref))
         return HttpResponse('<pre>Hook Started</pre>\n')
         
     raise Http404
