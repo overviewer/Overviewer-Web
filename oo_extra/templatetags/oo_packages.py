3b0f29df9f697171be8db8614f2a4c7441884477
Andrew Chin <achin@eminence32.net>
2013-07-04 11:49:17 -0400

    Changed Windows section of /downloads page to use BB builds

--- oo_extra/templatetags/oo_packages.py
+++ oo_extra/templatetags/oo_packages.py
@@ -1,5 +1,5 @@
 from django import template
-from oo_extra.models import Package
+from oo_extra.models import Package, Build
 
 DEFAULT_REPO = "git://github.com/overviewer/Minecraft-Overviewer.git"
 DEFAULT_CHECKOUT = "master"
@@ -9,6 +9,14 @@ register = template.Library()
 @register.assignment_tag
 def get_latest_package(platform, repo=DEFAULT_REPO, checkout=DEFAULT_CHECKOUT):
     pkgs = Package.objects.filter(platform=platform, repo=repo, checkout=checkout)
+    try:
+        return pkgs.order_by('-date')[0]
+    except IndexError:
+        return None
+
+@register.assignment_tag
+def get_latest_build(builder, repo=DEFAULT_REPO, branch=DEFAULT_CHECKOUT):
+    pkgs = Build.objects.filter(builder=builder, branch=branch)
     try:
         return pkgs.order_by('-date')[0]
     except IndexError:
