b4a50fdf3aab6631a1d57e04c8db46710ba4b58d
Aaron Griffith <aargri@gmail.com>
2015-03-21 17:57:25 -0400

    added shortcuts to flask app

--- flask/overviewer/views.py
+++ flask/overviewer/views.py
@@ -46,3 +46,18 @@ def token_getter():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Shortcuts...
+def shortcut(paths, destination):
+    for path in paths:
+        n1 = 'shortcut' + path.replace('/', '_')
+        n2 = 'shortcut_path' + path.replace('/', '_')
+        app.add_url_rule(path, n1, lambda: redirect(destination))
+        app.add_url_rule(path + '/<path:path>', n2, lambda path: redirect(destination + '/' + path))
+
+shortcut(['/wiki'], 'https://github.com/overviewer/Minecraft-Overviewer/wiki')
+shortcut(['/issues'], 'https://github.com/overviewer/Minecraft-Overviewer/issues')
+shortcut(['/code', '/git'], 'https://github.com/overviewer/Minecraft-Overviewer')
+shortcut(['/pulls'], 'https://github.com/overviewer/Minecraft-Overviewer/pulls')
+shortcut(['/pull'], 'https://github.com/overviewer/Minecraft-Overviewer/pull')
+shortcut(['/doc', '/docs'], 'http://docs.overviewer.org/')
