433731daab373669808102d6b00aa6ed999a5c16
Aaron Griffith <aargri@gmail.com>
2015-03-21 18:06:33 -0400

    added debian/rpm info shortcuts, and /donate to urls.txt

--- flask/overviewer/views.py
+++ flask/overviewer/views.py
@@ -55,9 +55,14 @@ def shortcut(paths, destination):
         app.add_url_rule(path, n1, lambda: redirect(destination))
         app.add_url_rule(path + '/<path:path>', n2, lambda path: redirect(destination + '/' + path))
 
+# shortcuts to github!
 shortcut(['/wiki'], 'https://github.com/overviewer/Minecraft-Overviewer/wiki')
 shortcut(['/issues'], 'https://github.com/overviewer/Minecraft-Overviewer/issues')
 shortcut(['/code', '/git'], 'https://github.com/overviewer/Minecraft-Overviewer')
 shortcut(['/pulls'], 'https://github.com/overviewer/Minecraft-Overviewer/pulls')
 shortcut(['/pull'], 'https://github.com/overviewer/Minecraft-Overviewer/pull')
+
+# shortcuts to RTD
 shortcut(['/doc', '/docs'], 'http://docs.overviewer.org/')
+shortcut(['/debian/info'], 'http://docs.overviewer.org/en/latest/installing/#debian-ubuntu')
+shortcut(['/rpms/info'], 'http://docs.overviewer.org/en/latest/installing/#centos-rhel-fedora')
