2bb7fbb0f47f9b4a21d2771038d94f2a299a368e
Aaron Griffith <aargri@gmail.com>
2015-03-25 20:55:56 -0400

    force flask and flask-migrate to use the same database for sure

--- flask/overviewer/settings/default.py
+++ flask/overviewer/settings/default.py
@@ -1,2 +1,6 @@
+import os.path
+
+ovr = os.path.join(os.path.split(__file__)[0], '..')
+
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///overviewer.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.abspath(os.path.join(ovr, '..', 'overviewer.db'))
