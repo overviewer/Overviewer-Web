2254174cb50ec4842ee2f4a9c4e1a868f5d42994
Aaron Griffith <aargri@gmail.com>
2015-06-06 09:05:04 -0400

    debian packages are now signed

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-No time to explain!
-
-    docker-compose up
-
-But first some confidential files need making. Copy over and modify
-the following example files:
+First some confidential files need making. Copy over and modify the
+following example files:
 
  * confidential.env.example
+
+You will also need to place an ASCII-armored private GPG key dump in:
+
+ * buildbot/codesign.asc
+
+Once those are in place, you can rebuild the images and bring
+everything online with:
+
+    docker-compose build
+    docker-compose up -d
--- buildbot/start.sh
+++ buildbot/start.sh
@@ -6,8 +6,14 @@ if [ ! -d "master" ]; then
     buildbot create-master master
 fi
 
-cp /root/{master.cfg,confidential.py,debian.tar} master/
+cp /root/{master.cfg,debian.tar} master/
 rm -f master/twistd.pid master/twistd.log
 mkdir -p uploads
 buildbot upgrade-master master
+
+eval $(gpg-agent --daemon --default-cache-ttl 1000000000 --max-cache-ttl 1000000000 --allow-preset-passphrase)
+/usr/lib/gnupg2/gpg-preset-passphrase -P $CODESIGN_PASSPHRASE -c $CODESIGN_KEYGRIP
+unset CODESIGN_PASSPHRASE
+unset CODESIGN_KEYGRIP
+
 exec buildbot start --nodaemon master
