20b18e3eaabf0a80e30ad2dc2afca372f9b4be39
Aaron Griffith <aargri@gmail.com>
2015-06-07 11:55:04 -0400

    windows builds

--- buildbot/start.sh
+++ buildbot/start.sh
@@ -10,6 +10,11 @@ cp /root/{master.cfg,debian.tar,rpm.tar} master/
 rm -f master/twistd.pid master/twistd.log
 buildbot upgrade-master master
 
+eval $(gpg-agent --daemon --default-cache-ttl 1000000000 --max-cache-ttl 1000000000 --allow-preset-passphrase)
+/usr/lib/gnupg2/gpg-preset-passphrase -P $CODESIGN_PASSPHRASE -c $CODESIGN_KEYGRIP
+unset CODESIGN_PASSPHRASE
+unset CODESIGN_KEYGRIP
+
 mkdir -p uploads
 
 mkdir -p repos/debian/files
@@ -26,10 +31,5 @@ ln -fs 16 repos/rpm/20
 ln -fs 16 repos/rpm/21
 ln -fs 21 repos/rpm/latest
 make -C repos/rpm/
-
-eval $(gpg-agent --daemon --default-cache-ttl 1000000000 --max-cache-ttl 1000000000 --allow-preset-passphrase)
-/usr/lib/gnupg2/gpg-preset-passphrase -P $CODESIGN_PASSPHRASE -c $CODESIGN_KEYGRIP
-unset CODESIGN_PASSPHRASE
-unset CODESIGN_KEYGRIP
 
 exec buildbot start --nodaemon master
