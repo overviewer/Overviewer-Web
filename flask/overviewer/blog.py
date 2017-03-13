ce78d4fd5d38caf5484ef1bb70483f07caeede6c
Aaron Griffith <aargri@gmail.com>
2017-03-12 20:28:22 -0400

    cheap attempt at better blog caching

--- flask/overviewer/blog.py
+++ flask/overviewer/blog.py
@@ -55,7 +55,6 @@ class PostForm(Form):
     title = StringField('title', validators=[DataRequired()])
     body = TextAreaField('body')
 
-@cache.cached(1800)
 def index_data_for(query):
     try:
         page = int(request.args.get('page', 1))
@@ -69,21 +68,25 @@ def index_for(query, tmpl='blog_index.html'):
     return render_blog(tmpl, posts=posts.items, page=posts)
 
 @app.route('/blog/')
+@cache.cached(1800)
 def blog_index():
     return index_for(BlogPost.query)
 
 @app.route('/blog/feeds/latest/')
 @content_type('application/rss+xml')
+@cache.cached(1800)
 def blog_rss():
     return index_for(BlogPost.query, tmpl='blog_rss.xml')
 
 @app.route('/blog/<int:year>/')
+@cache.cached(1800)
 def blog_index_year(year):
     start = datetime(year=year, month=1, day=1)
     end = datetime(year=year + 1, month=1, day=1)
     return index_for(BlogPost.query.filter(BlogPost.timestamp.between(start, end)))
 
 @app.route('/blog/<int:year>/<int:month>/')
+@cache.cached(1800)
 def blog_index_month(year, month):
     start = datetime(year=year, month=month, day=1)
     if month == 12:
@@ -93,6 +96,7 @@ def blog_index_month(year, month):
     return index_for(BlogPost.query.filter(BlogPost.timestamp.between(start, end)))
 
 @app.route('/blog/<int:year>/<int:month>/<int:day>/')
+@cache.cached(1800)
 def blog_index_day(year, month, day):
     start = datetime(year=year, month=month, day=day)
     end = start + timedelta(days=1)
