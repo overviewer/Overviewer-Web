4f88bd49b6a2629010694093e13aa95821df4f74
Nicolas F <ovdev@fratti.ch>
2019-03-22 22:53:41 +0100

    fix manage.py imports

--- flask/manage.py
+++ flask/manage.py
@@ -1,5 +1,5 @@
-from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 from overviewer.app import app
 from overviewer.models import db
