45e0cfc2848b47cdcbdc965a2cba98ea141346d9
Ben Steadman <steadmanben1@gmail.com>
2019-04-14 13:40:49 +0100

    restructure buildbot files to reduce moving files in scripts

--- buildbot/start.sh
+++ buildbot/start.sh
@@ -6,7 +6,7 @@ if [ ! -d "master" ]; then
     buildbot create-master master
 fi
 
-cp /root/{master.cfg,debian.tar,rpm.tar,pyinstaller-windows.spec} master/
+cp /root/master/* master/
 rm -f master/twistd.pid master/twistd.log
 buildbot upgrade-master master
 
@@ -17,27 +17,26 @@ unset CODESIGN_KEYGRIP
 
 mkdir -p uploads renders
 
-mkdir -p repos/debian/files
-cp /root/debian-repo/* repos/debian/
-make -C repos/debian/
+mkdir -p repos/debian/repo/files
+cp -r /root/repos/debian repos/
+make -C repos/debian/repo
 
-mkdir -p repos/rpm
-cp /root/rpm-repo/* repos/rpm/
-mkdir -p repos/rpm/{6,7}/{i386,x86_64}/packages
-ln -fs 7 repos/rpm/16
-ln -fs 7 repos/rpm/17
-ln -fs 7 repos/rpm/18
-ln -fs 7 repos/rpm/19
-ln -fs 7 repos/rpm/20
-ln -fs 7 repos/rpm/21
-ln -fs 7 repos/rpm/22
-ln -fs 7 repos/rpm/23
-ln -fs 7 repos/rpm/24
-ln -fs 7 repos/rpm/25
-ln -fs 7 repos/rpm/26
-ln -fs 7 repos/rpm/27
-ln -fs 7 repos/rpm/28
-ln -fs 7 repos/rpm/latest
-make -C repos/rpm/
+cp -r /root/repos/rpm repos/
+mkdir -p repos/rpm/repo/{6,7}/{i386,x86_64}/packages
+ln -fs 7 repos/rpm/repo/16
+ln -fs 7 repos/rpm/repo/17
+ln -fs 7 repos/rpm/repo/18
+ln -fs 7 repos/rpm/repo/19
+ln -fs 7 repos/rpm/repo/20
+ln -fs 7 repos/rpm/repo/21
+ln -fs 7 repos/rpm/repo/22
+ln -fs 7 repos/rpm/repo/23
+ln -fs 7 repos/rpm/repo/24
+ln -fs 7 repos/rpm/repo/25
+ln -fs 7 repos/rpm/repo/26
+ln -fs 7 repos/rpm/repo/27
+ln -fs 7 repos/rpm/repo/28
+ln -fs 7 repos/rpm/repo/latest
+make -C repos/rpm/repo
 
 exec buildbot start --nodaemon master
