82cf74cfca39ff32fbe7010c001c7b3fe1963bd0
Ben Steadman <steadmanben1@gmail.com>
2019-04-14 20:08:54 +0100

    parameterise rpmsign gpg name

--- buildbot/rpmsign.sh
+++ buildbot/rpmsign.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rpm --addsign \
---define "_gpg_name overviewer.org" \
+--define "_gpg_name $CODESIGN_NAME" \
 --define "__gpg_check_password_cmd /bin/true" \
 --define "__gpg_sign_cmd %{__gpg} gpg --batch --no-verbose --no-armor --use-agent -u %{_gpg_name} --no-secmem-warning -sbo %{__signature_filename} %{__plaintext_filename}" \
 $1
