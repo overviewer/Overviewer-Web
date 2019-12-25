280ab756f4b159f4714bd75f4c1c130a6dfd1865
Aaron Griffith <aargri@gmail.com>
2019-12-24 22:20:55 -0500

    update to new postgres container, fix rpm signing

--- buildbot/rpmsign.sh
+++ buildbot/rpmsign.sh
@@ -2,5 +2,6 @@
 rpm --addsign \
 --define "_gpg_name $CODESIGN_NAME" \
 --define "__gpg_check_password_cmd /bin/true" \
+--define "__gpg /usr/bin/gpg" \
 --define "__gpg_sign_cmd %{__gpg} gpg --batch --no-verbose --no-armor --use-agent -u %{_gpg_name} --no-secmem-warning -sbo %{__signature_filename} %{__plaintext_filename}" \
 $1
