3b725d12c702a4a814083ad113cf8e79045e9438
Nicolas F <ovdev@fratti.ch>
2019-12-27 20:50:04 +0100

    Fix rendering, db init, example
    A bunch of small fixes:
    - update client jar version to 1.15
    - the newer version of the postgres container no longer uses
      gosu, you can just invoke the commands directly
    - link example correctly, because adding a string and a Path() is
      not a thing you can do but somehow nothing ever yelled at us

--- postgres/init-database.sh
+++ postgres/init-database.sh
@@ -1,7 +1,7 @@
-gosu postgres postgres --single -jE <<EOF
+psql <<EOF
 CREATE DATABASE "flask" WITH OWNER="postgres" TEMPLATE=template0 ENCODING='UTF8';
 EOF
 
-gosu postgres postgres --single -jE <<EOF
+psql <<EOF
 CREATE DATABASE "bbmaster" WITH OWNER="postgres" TEMPLATE=template0 ENCODING='UTF8';
 EOF
